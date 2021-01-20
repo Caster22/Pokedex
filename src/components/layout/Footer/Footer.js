2d845a99cdb623aa55bf1f5f94722d9f7d914908
Michał Kopacz <kopaczmichal093@gmail.com>
2021-01-20 20:28:28 +0100

    Add elements and styles for Footer component

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -5,7 +5,19 @@ import styles from './Footer.module.scss';
 const Component = () => {
   return (
     <footer className={ styles.root }>
-      Footer
+      <div className='container'>
+        <div className='row py-2'>
+          <div className='col-12 col-md-6 text-md-right text-center'>
+            Copyright Ⓒ 2021
+          </div>
+          <div className='col-12 col-md-6 text-md-left text-center'>
+            Fake-Mardesoft-Fake
+          </div>
+          <div className='col-12 text-center'>
+            All rights reserved
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
